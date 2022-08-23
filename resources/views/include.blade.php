11bc44785f67110d771d6415baed3c641631c950
Lucas Vieira <vieira07lucas@gmail.com>
2022-08-23 11:28:14 -0300

    Entendendo as variacoes do Include

--- resources/views/include.blade.php
+++ resources/views/include.blade.php
@@ -1,6 +1,6 @@
 <h1> Includes Hello World </h1>
 
-@component('components.component')
-    <h1> Componente 1 - HTML </h1>
-    <p> Um texto qualquer </p>
-@endcomponent
+@includeFirst(
+['components.component2', 'components.component1'],
+['html' => '<h1>Componente1</h1>']
+)
